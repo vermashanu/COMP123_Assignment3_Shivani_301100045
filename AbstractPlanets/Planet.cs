85a10e934b0a3805dcdc9be741a2d3f7de9c4064
vermashanu <insanshanu11@gmail.com>
2020-07-24 03:12:30 -0400

    Add project files.

---
+++ AbstractPlanets/Planet.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPlanets
+{
+    /// <summary>
+    /// This is an abstract class to hold
+    /// detail information about a Planet.
+    /// </summary>
+    public abstract class Planet
+    {
+        // PRIVATE MEMBERS
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        // PUBLIC PROPERTIES
+        public double Diameter
+        {
+            get { return _diameter; }
+            set { _diameter = value; }
+        }
+
+        public double Mass
+        {
+            get { return _mass; }
+            set { _mass = value; }
+        }
+
+        public int MoonCount
+        {
+            get { return _moonCount; }
+            set { _moonCount = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public double OrbitalPeriod
+        {
+            get { return _orbitalPeriod; }
+            set { _orbitalPeriod = value; }
+        }
+
+        public int RingCount
+        {
+            get { return _ringCount; }
+            set { _ringCount = value; }
+        }
+
+        public double RotationPeriod
+        {
+            get { return _rotationPeriod; }
+            set { _rotationPeriod = value; }
+        }
+
+        /// <summary>
+        /// Class constructor, takes name, diameter and mass
+        /// ass peremeter values and initialize member variables
+        /// _name, _diameter and _mass
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        public Planet(string name, double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
+
+        /// <summary>
+        /// Override default ToString() to display Planet's 
+        /// name, diameter and mass to console
+        /// </summary>
+        /// <returns>Formatted string as '[ name, diameter, mass ]'</returns>
+        public override string ToString()
+        {
+            return "[ " + Name + ", " + Diameter + ", " + Mass + " ]";
+        }
+    }
+}
---
+++ AbstractPlanets/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPlanets
+{
+    /// <summary>
+    /// This driver class create GiantPlanet and
+    /// TerrestrialPlanet object by calling constructor
+    /// and display detail info by calling ToString()
+    /// method.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Prompt user to press any key to exit
+        /// the program.
+        /// </summary>
+        static void waitForAnyKey()
+        {
+            Console.Write("\nPlease press any key to exit...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Create and display object details of GiantPlanet
+        /// and TerrestrialPlanet.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Create and display GiantPlanet object
+            GiantPlanet giantPlanet = 
+                new GiantPlanet("Jupiter", 139820.0, 1898000000000000000000000000.0, "Gas");
+            Console.WriteLine("Giant Planet:");
+            Console.WriteLine(giantPlanet.ToString());
+
+            // Create and display TerrestrialPlanet object
+            TerrestrialPlanet terrestrialPlanet = 
+                new TerrestrialPlanet("Earth", 12742.0, 5972000000000000000000000.0, true);
+            Console.WriteLine("\nTerrestrial Planet:");
+            Console.WriteLine(terrestrialPlanet.ToString());
+
+            // Call waitForAnyKey() method
+            waitForAnyKey();
+        }
+    }
+}
